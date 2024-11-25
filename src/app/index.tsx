7c6b93d996980862686d39b60891d07c72f747ec
RobertBatista <robertbatista@e-get.com.br>
2024-11-25 00:45:18 -0300

    bugfix: correção da exibição

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,15 @@
-import { Dispatch, useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { HiArrowSmLeft, HiArrowSmRight } from "react-icons/hi";
 
-import { StateUpdater } from "preact/hooks";
+import { AxiosError, AxiosResponse } from "axios";
 
-import { TPokemon } from "../types/TPokemon";
+import ProgressBar from "../components/ProgressBar";
+import Detail from "../components/Detail";
+
+import { TPokemon, TPokemonList } from "../types/TPokemon";
 import { TRow } from "../types/TRow";
+import { TPokemonSpecie } from "../types/TPokemonSpecie";
+import { TEvolutionChain, TEvolvesTo } from "../types/TEvolutionChain";
 
 import { TypeColor } from "../enums/TypeColor";
 
@@ -18,69 +23,193 @@ import {
   MainDashboard,
   NavbarDashboard,
   TooltipDashboard,
+  AboutPokemon,
+  CardPokemon,
+  ContentPokemon,
+  MainPokemon,
+  StatusPokemon,
   ChipText,
   Chip,
 } from "./styles";
 
-import LoadingPikachu from "./LoadingPikachu";
-import PokemonSpecie from "./PokemonSpecie";
-
-interface useFetchPokemonProps {
-  setLoading: Dispatch<StateUpdater<boolean>>;
-}
-
-function useFetchPokemon({ setLoading }: useFetchPokemonProps) {
-  const [url, setUrl] = useState<string>("https://pokeapi.co/api/v2/pokemon");
+export default function App() {
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
   const [pokemonList, setPokemonList] = useState<Array<TPokemon>>([]);
-  const [prevUrl, setPrevUrl] = useState<string>("");
-  const [nextUrl, setNextUrl] = useState<string>("");
+  const [pokemon, setPokemon] = useState<TPokemon>();
+  const [prevUrl, setPrevUrl] = useState("");
+  const [nextUrl, setNextUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleFetchPokemon = useCallback((responseData: TPokemonList) => {
+    setPokemonList([]);
+
+    responseData.results.forEach(async (pokemon: TRow) => {
+      await request({ url: pokemon.url })
+        .then((res: AxiosResponse<TPokemon>) => {
+          setPokemonList((prevState) => [...prevState, res.data]);
+        })
+        .catch((error: AxiosError) => {
+          console.error(error);
+        });
+    });
+  }, []);
 
   const handleFetchListPokemon = useCallback(async () => {
     setLoading(true);
 
-    try {
-      const response = await request({ url: url });
+    await request({ url: url })
+      .then((res: AxiosResponse) => {
+        setPrevUrl(res.data.previous);
+        setNextUrl(res.data.next);
 
-      setPrevUrl(response.data.previous);
-      setNextUrl(response.data.next);
-
-      const pokemonList: TPokemon[] = [];
-
-      response.data.results.forEach(async (pokemon: TRow) => {
-        const response = await request({ url: pokemon.url });
-
-        pokemonList.push(response.data);
+        handleFetchPokemon(res.data);
+      })
+      .catch((error: AxiosError) => {
+        console.error(error);
       });
 
-      setPokemonList(pokemonList);
-    } catch (error) {
-      console.error(error);
-    }
-
-    setInterval(() => setLoading(false), 1000);
-  }, [url]);
+    setInterval(() => {
+      setLoading(false);
+    }, 1000);
+  }, [handleFetchPokemon, url]);
 
   useEffect(() => {
     handleFetchListPokemon();
   }, [handleFetchListPokemon]);
 
-  return { pokemonList, prevUrl, nextUrl, setUrl };
-}
-
-function App() {
-  const [pokemon, setPokemon] = useState<TPokemon>();
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const { pokemonList, prevUrl, nextUrl, setUrl } = useFetchPokemon({
-    setLoading,
-  });
-
-  if (!!loading) {
-    return <LoadingPikachu />;
+  if (loading) {
+    return (
+      <MainDashboard>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <h1
+              style={{
+                fontFamily: "Pixelify Sans, sans-serif",
+                fontStyle: "normal",
+              }}
+            >
+              Carregando...
+            </h1>
+          </div>
+        </div>
+      </MainDashboard>
+    );
   }
 
-  if (!!pokemon) {
-    return <PokemonSpecie pokemon={pokemon} setPokemon={setPokemon} />;
+  if (pokemon) {
+    // @ts-ignore
+    const color = TypeColor[pokemon.types[0].type.name];
+
+    return (
+      <MainPokemon>
+        <CardPokemon css={{ $$bgColor: color }}>
+          <button
+            style={{
+              color: "#fff",
+              fontWeight: "500",
+              display: "flex",
+              alignItems: "center",
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+            }}
+            type="button"
+            onClick={() => {
+              setPokemon(undefined);
+            }}
+          >
+            <HiArrowSmLeft size={20} />
+            Pokédex
+          </button>
+
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: "2rem",
+            }}
+          >
+            <img
+              title={pokemon.name}
+              src={pokemon.sprites.other["official-artwork"].front_default}
+              alt={pokemon.name}
+              style={{ maxWidth: "200px" }}
+            />
+
+            <div style={{ textTransform: "capitalize", color: "#fff" }}>
+              <div style={{ marginBottom: "1rem" }}>
+                <small style={{ fontWeight: "500" }}>
+                  # {String(pokemon.id).padStart(4, "0")}
+                </small>
+
+                <h2>{pokemon.name}</h2>
+              </div>
+
+              {pokemon.types.map((item) => {
+                return (
+                  <Chip key={item.type.name}>
+                    <ChipText>{item.type.name}</ChipText>
+                  </Chip>
+                );
+              })}
+            </div>
+          </div>
+        </CardPokemon>
+
+        <ContentPokemon>
+          <AboutPokemon>
+            <Detail type={"hg"} value={pokemon.height} name={"Height"} />
+            <Detail type={"dm"} value={pokemon.weight} name={"Weight"} />
+            <Detail
+              type={"xp"}
+              value={pokemon.base_experience}
+              name={"Base experience"}
+            />
+          </AboutPokemon>
+        </ContentPokemon>
+
+        <ContentPokemon>
+          <StatusPokemon>
+            <h2 style={{ textAlign: "center", marginBottom: "1rem" }}>
+              Base status
+            </h2>
+
+            {pokemon.stats.map((item) => {
+              return (
+                <ProgressBar
+                  key={item.stat.name}
+                  name={item.stat.name}
+                  value={+item.base_stat}
+                  max={300}
+                />
+              );
+            })}
+          </StatusPokemon>
+        </ContentPokemon>
+
+        <ContentPokemon>
+          <h2 style={{ textAlign: "center", marginBottom: "1rem" }}>
+            Evolution Chain
+          </h2>
+
+          <PokemonSpecie specie={pokemon.species} />
+        </ContentPokemon>
+      </MainPokemon>
+    );
   }
 
   return (
@@ -116,7 +245,15 @@ function App() {
             <CardDashboard
               key={value.id}
               css={{ $$bgColor: color }}
-              onClick={() => setPokemon(value)}
+              onClick={() => {
+                setLoading(true);
+
+                setPokemon(value);
+
+                setInterval(() => {
+                  setLoading(false);
+                }, 1000);
+              }}
             >
               <div style={{ textTransform: "capitalize", color: "white" }}>
                 <h4 style={{ marginBottom: "1rem" }}>{value.name}</h4>
@@ -159,4 +296,62 @@ function App() {
   );
 }
 
-export default App;
+interface PokemonSpecieProps {
+  specie: { name: string; url: string };
+}
+
+function PokemonSpecie({ specie }: PokemonSpecieProps) {
+  const [evolutionChain, setEvolutionChain] = useState<TEvolutionChain>();
+
+  const handleFetchChain = useCallback(
+    async (evolution_chain: Omit<TRow, "name">) => {
+      await request({ url: evolution_chain.url })
+        .then((res: AxiosResponse<TEvolutionChain>) => {
+          setEvolutionChain(res.data);
+        })
+        .catch((error: AxiosError) => {
+          console.error(error);
+        });
+    },
+    []
+  );
+
+  const handleFetchSpecie = useCallback(async () => {
+    await request({ url: specie.url })
+      .then((res: AxiosResponse<TPokemonSpecie>) => {
+        handleFetchChain(res.data.evolution_chain);
+      })
+      .catch((error: AxiosError) => {
+        console.error(error);
+      });
+  }, [handleFetchChain, specie.url]);
+
+  useEffect(() => {
+    handleFetchSpecie();
+  }, [handleFetchSpecie]);
+
+  if (!evolutionChain?.chain.evolves_to) {
+    return <span>Não tem evolução</span>;
+  }
+
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <ul>
+        {evolutionChain?.chain.evolves_to.map((firstEvolution: TEvolvesTo) => {
+          return (
+            <li
+              style={{
+                textTransform: "capitalize",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              {evolutionChain?.chain.species.name} <HiArrowSmRight size={20} />
+              {firstEvolution.species.name}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
